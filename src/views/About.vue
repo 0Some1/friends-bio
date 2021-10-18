c74beed710decb10b3f45a51bf83c303a75c8cad
l_luciano <sda@das.xo>
2021-10-19 03:16:47 +0330

    about page is completed

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,25 @@
 <template>
-  <div class="about">
-    <h1 class="mt-5">About The Creator of this website</h1>
-    
+  <div class="mt-5 ">
+    <div class=" m-auto w-100">
+      <b-card
+          title="M.Sadegh Ghalayani"
+          img-src="https://picsum.photos/600/300/?image=20"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 20rem;"
+          class="shadow-lg m-auto"
+      >
+        <b-card-text>
+          This is a not legit website about Friends series and it was just meant to be for an assigment of my  Internet Engineering course.
+        </b-card-text>
+        <b-card-text>
+          E-mail: mghalayani@gmail.com
+        </b-card-text>
+
+        <b-button href="https://github.com/0Some1/friends-bio" variant="primary">Source Code</b-button>
+      </b-card>
+    </div>
+
   </div>
 </template>
